401d4bf34b7bc238a1a4fe9472814d433401ebf9
0cmat <mattcon022@gmail.com>
2021-05-02 14:29:19 -0300

    Desafio Semaforo

---
+++ Desafio - Semaforo/script.js
@@ -0,0 +1,14 @@
+var corSemaforo = document.getElementById('semaforo')
+function vermelho(){
+    corSemaforo.src = 'img/vermelho.png'
+}
+function amarelo(){
+    corSemaforo.src = 'img/amarelo.png'
+}
+function verde(){
+    corSemaforo.src = 'img/verde.png'
+}
+
+function automatico(){
+  // falta implementar
+}
