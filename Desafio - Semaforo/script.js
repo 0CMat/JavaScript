c7443864e35eced372d21c011eb6f8ac9a99f098
0cmat <mattcon022@gmail.com>
2021-05-09 20:40:15 -0300

    Melhorado o código

--- Desafio - Semaforo/script.js
+++ Desafio - Semaforo/script.js
@@ -1,14 +1,7 @@
 var corSemaforo = document.getElementById('semaforo')
-function vermelho(){
-    corSemaforo.src = 'img/vermelho.png'
-}
-function amarelo(){
-    corSemaforo.src = 'img/amarelo.png'
-}
-function verde(){
-    corSemaforo.src = 'img/verde.png'
-}
+vermelho = () => corSemaforo.src = 'img/vermelho.png'
+amarelo = () => corSemaforo.src = 'img/amarelo.png'
+verde = () => corSemaforo.src = 'img/verde.png'
 
-function automatico(){
-  // falta implementar
-}
+
+
